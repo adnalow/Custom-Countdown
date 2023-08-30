f0231b6bdfdbf683be4bdf9097851b148a8ee3f2
Adrian <reinieradrianluna@gmail.com>
2023-08-30 15:48:51 +0800

    form values

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEL = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set date input min width today's date
 const today = new Date().toISOString().split('T') [0];
 dateEL.setAttribute('min', today);
+
+// Take values from input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
